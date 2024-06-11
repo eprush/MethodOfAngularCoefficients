553bb949397d50e660bdc47b011866a31b00e6b5
eprush <>
2024-06-11 19:26:58 +0300

    Add plot_zero_pos method

---
+++ MNK.py
@@ -0,0 +1,25 @@
+from math import sqrt
+import numpy as np
+
+
+class Interpolator:
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+
+    def calc_params(self) -> tuple[float, float, float, float]:
+        n = len(self.y)
+        aver_y, aver_x = np.mean(self.y), np.mean(self.x)
+        aver_xy, aver_yy, aver_xx = np.mean(self.x * self.y), np.mean(self.y ** 2), np.mean(self.x ** 2)
+        A = (aver_xy - aver_x * aver_y) / (aver_xx - aver_x ** 2)
+        B = aver_y - A * aver_x
+        err_A = sqrt(((aver_yy - aver_y ** 2) / (aver_xx - aver_x ** 2) - A ** 2) / n)
+        err_B = err_A * sqrt(aver_xx - aver_x ** 2)
+        return A, B, err_A, err_B
+
+    def calc_line(self):
+        a, b, *_ = self.calc_params()
+        return a * self.x + b
+
+    def calc_error(self):
+        *_, err_a, err_b = self.calc_params()
+        return np.sqrt((self.x * err_a) ** 2 + err_b ** 2)
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from numpy.typing import NDArray
-from scipy.optimize import fsolve
-from scipy.interpolate import BarycentricInterpolator as Interpolator
+from MNK import Interpolator
 from separator.round import RoundSeparator
 from theoretical_cc import theoretical_cc
 
@@ -14,6 +13,7 @@ def plot_num_of_cells(R, L, nums: NDArray[int]):
             s = RoundSeparator(R, L, nums[i])
             coeffs = s.calc_ac()
             Y[i] = s.calc_clausing(coeffs)
+        print(Y[-1])
         return Y
 
     plt.xlabel("Количество ячеек n", size=17)
@@ -42,8 +42,8 @@ class Graph:
             plt.plot(self.lens, self.graphs[num], label=f"{num}")
         if k is not None and len(k):
             plt.plot(self.lens, k, label=f"теория")
-        plt.xlabel("Длина канала L/d", size=17)
-        plt.ylabel("Коэффициент Клаузинга", size=17)
+        plt.xlabel("Длина канала L/d", size=19)
+        plt.ylabel("Коэффициент Клаузинга", size=19)
         plt.legend(title="Количество колец")
         plt.grid()
         plt.show()
@@ -59,8 +59,8 @@ class Graph:
         lens_min, deviation_min = [], []
         for num in self.nums:
             plt.plot(self.lens, self.calc_deviation(num, k), label=f"{num}")
-        plt.xlabel("Длина канала L/d", size=17)
-        plt.ylabel("Отклонение КК, %", size=17)
+        plt.xlabel("Длина канала L/d", size=19)
+        plt.ylabel("Отклонение КК, %", size=19)
         plt.legend(title="Количество колец")
         plt.grid()
         plt.show()
@@ -87,24 +87,28 @@ class Graph:
         return
 
     def plot_zero_pos(self, k: NDArray = None):
-        zeros = []
-        for num in self.nums:
-            deviation = self.calc_deviation(num, k)
-            func = Interpolator(np.array(self.lens), deviation)
-            root = fsolve(func, np.zeros(1))[-1]
-            if num == 30:
-                print(root)
-            zeros.append(root)
-        plt.plot(self.nums, zeros)
+        zeros = np.zeros(len(self.nums), dtype=float)
+        for i in range(len(self.nums)):
+            deviation = self.calc_deviation(self.nums[i], k)
+            for j in range(len(deviation)):
+                if deviation[j] >= 0.0 and j < len(deviation) - 1:
+                    zeros[i] = self.lens[j]
+                    break
+        interp = Interpolator(self.nums, zeros)
+        print(interp.calc_params())
+        plt.errorbar(self.nums, zeros, yerr=interp.calc_error(), linestyle=" ")
+        plt.plot(self.nums, zeros, marker="o", markersize=1.5, linestyle=" ", label="эксп. точки")
+        plt.plot(self.nums, interp.calc_line(), label="аппроксимация")
         plt.xlabel("Количество колец", size=17)
         plt.ylabel("Положение нуля", size=17)
+        plt.legend()
         plt.grid()
         plt.show()
-        return
 
 
 # plot_num_of_cells(1, 2, np.arange(1, 31))
-y = np.arange(9, 50)
+y = np.arange(1, 55)
+# plot_num_of_cells(1, 50, y)
 g = Graph(list(theoretical_cc.keys()), y)
 # g.plot_clausing(np.array(list(theoretical_cc.values())))
 # g.plot_deviation(np.array(list(theoretical_cc.values())))
